c4467add03fc9bbf88394fe5b948403392f93052
Robin De Schepper <robin.deschepper93@gmail.com>
2023-12-15 11:36:41 +0100

    b2 - switch to beta stack

--- bsb_neuron/__init__.py
+++ bsb_neuron/__init__.py
@@ -7,5 +7,5 @@ from .adapter import NeuronAdapter
 from .simulation import NeuronSimulation
 from . import devices
 
-__version__ = "0.0.0b1"
+__version__ = "0.0.0b2"
 __plugin__ = SimulationBackendPlugin(Simulation=NeuronSimulation, Adapter=NeuronAdapter)
