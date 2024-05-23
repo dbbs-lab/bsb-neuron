4fc632ce1e8306d07a351b34cb1fbd30222742a3
filimarc <52816133+filimarc@users.noreply.github.com>
2024-05-23 09:57:42 +0200

    Fix voltage clamp (#13) |  Bump version: 4.0.0 → 4.0.1
    * Fix implement method for Voltage Clamp
    
    Arguments that are passed were not the standard one
    
    * Fix Voltage clamp
    
    redo
    
    * Fix black
    
    * Bump version: 4.0.0 → 4.0.1

--- bsb_neuron/__init__.py
+++ bsb_neuron/__init__.py
@@ -8,5 +8,5 @@ from . import devices
 from .adapter import NeuronAdapter
 from .simulation import NeuronSimulation
 
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 __plugin__ = SimulationBackendPlugin(Simulation=NeuronSimulation, Adapter=NeuronAdapter)
