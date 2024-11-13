032f977e5f3dc6c81533688432eff746b45a0d54
Filippo <52816133+filimarc@users.noreply.github.com>
2024-11-13 09:30:47 +0100

    Added metadata to have more info when loading result nio files (#22)
    Co-authored-by: danilobenozzo <danilo.benozzo@gmail.com>

--- bsb_neuron/devices/voltage_recorder.py
+++ bsb_neuron/devices/voltage_recorder.py
@@ -13,9 +13,15 @@ class VoltageRecorder(NeuronDevice, classmap_entry="voltage_recorder"):
         ).items():
             for target in pop:
                 for location in self.locations.get_locations(target):
-                    self._add_voltage_recorder(simdata.result, location)
+                    self._add_voltage_recorder(
+                        simdata.result,
+                        location,
+                        name=self.name,
+                        cell_type=target.cell_model.name,
+                        cell_id=target.id,
+                    )
 
-    def _add_voltage_recorder(self, results, location):
+    def _add_voltage_recorder(self, results, location, **annotations):
         section = location.section
         x = location.arc(0)
-        results.record(section(x)._ref_v)
+        results.record(section(x)._ref_v, **annotations)
